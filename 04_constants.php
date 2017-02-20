ce7c668041a720394a97fc76351e060123acbf28
CedricS-Immalle <cedric.sas@immalle.be>
2017-02-20 10:02:50 +0100

    oef 5 constants

--- 04_constants.php
+++ 04_constants.php
@@ -5,5 +5,10 @@ define("USERNAME", "joske");
 echo USERNAME;
 
 // definieer ook een constante PASSWORD en SERVERNAME
-
+define("PASSWORD", "jos");
+echo "<br>";
+echo PASSWORD;
+define("SERVERNAME", "mysql haxbench");
+echo "<br>";
+echo SERVERNAME;
 ?>
