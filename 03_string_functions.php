7183dac029b076c02d3ceabe0323a63cc1ecde66
CedricS-Immalle <cedric.sas@immalle.be>
2017-02-20 09:54:05 +0100

    oef 4 stringfuncties

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -1,11 +1,13 @@
 <?php
 
-$zin = "Binnenkort is het weeral lente";
+$zin = "Binnenkort is het weeral lente ";
 
 // Print af hoeveel woorden $zin bevat.
-
+echo str_word_count ($zin);
 
 // Vervang in $zin "lente" door "zomer" en druk af.
-
+$zin2 = str_ireplace("lente" , "zomer" , $zin);
+echo "<br>";
+echo $zin2;
 
 ?>
