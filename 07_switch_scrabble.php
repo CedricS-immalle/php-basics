d8fe5963d21698dbf4e9c947e6ed512f2b5ec3e1
cedricS-immalleq <cedric.sas@immalle.be>
2017-02-22 15:53:33 +0100

    oef7

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -14,6 +14,38 @@ $punten = 0;
 // R 2 punten
 // I 1 punt
 // N 1 punt
+for ($i=0; $i <= strlen($woord) ; $i++) { 
+    
 
-
+switch ($woord[$i]) {
+    case 'q':
+        $punten +=10;
+        break;
+    case 'u':
+        $punten +=4;
+        break;
+        case 'o':
+        $punten +=1;
+        break;
+        case 't':
+        $punten +=2;
+        break;
+        case 'e':
+        $punten +=1;
+        break;
+        case 'r':
+        $punten +=2;
+        break;
+        case 'i':
+        $punten +=1;
+        break;
+        case 'n':
+        $punten +=1;
+        break;
+    
+        
+        
+}
+}
+echo $punten
 ?>
